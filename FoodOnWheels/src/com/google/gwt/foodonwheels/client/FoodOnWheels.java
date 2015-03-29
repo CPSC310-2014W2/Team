c221387a567720ae540e65c53d6999519a08b91c
Yongnan (Devin) Li <devinli05@gmail.com>
2015-03-29 01:13:01 -0700

    Add CellTable to display FoodTruck with name and user checkin count, can sort name or count. Add getUrl and getCount methods to FoodTruckData to access the new info on website url and user checkin count.

--- FoodOnWheels/src/com/google/gwt/foodonwheels/client/FoodOnWheels.java
+++ FoodOnWheels/src/com/google/gwt/foodonwheels/client/FoodOnWheels.java
@@ -2,6 +2,7 @@ package com.google.gwt.foodonwheels.client;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 import com.gargoylesoftware.htmlunit.javascript.host.geo.Coordinates;
@@ -31,11 +32,15 @@ import com.google.gwt.event.dom.client.KeyCodes;
 import com.google.gwt.event.dom.client.KeyUpEvent;
 import com.google.gwt.event.dom.client.KeyUpHandler;
 import com.google.gwt.user.cellview.client.CellList;
+import com.google.gwt.user.cellview.client.CellTable;
+import com.google.gwt.user.cellview.client.ColumnSortEvent.ListHandler;
 import com.google.gwt.user.cellview.client.HasKeyboardSelectionPolicy.KeyboardSelectionPolicy;
+import com.google.gwt.user.cellview.client.TextColumn;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Anchor;
 import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.DecoratorPanel;
 import com.google.gwt.user.client.ui.DialogBox;
 import com.google.gwt.user.client.ui.DockLayoutPanel;
 import com.google.gwt.user.client.ui.HTML;
@@ -43,8 +48,11 @@ import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootLayoutPanel;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.ScrollPanel;
+import com.google.gwt.user.client.ui.TabLayoutPanel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.view.client.ListDataProvider;
 import com.google.gwt.view.client.SelectionChangeEvent;
 import com.google.gwt.view.client.SingleSelectionModel;
 
@@ -60,16 +68,21 @@ public class FoodOnWheels implements EntryPoint {
 			+ "attempting to contact the server. Please check your network "
 			+ "connection and try again.";
 
-	/**
-	 * Create a remote service proxy to talk to the server-side Greeting
-	 * service.
-	 */
-
 	// private HorizontalPanel mainPanel = new HorizontalPanel();
 	private VerticalPanel truckListPanel = new VerticalPanel();
 	private Button fetchTruckListButton = new Button("fetch YELP data");
-	private CellList<String> truckCellList = new CellList<String>(
-			new TextCell());
+	
+    private TabLayoutPanel tabLayout = new TabLayoutPanel(2.5, Unit.EM);
+
+	private final ListDataProvider<FoodTruckData> 
+	truckDataProvider = new ListDataProvider<FoodTruckData>();
+	private CellTable<FoodTruckData> 
+	truckCellTable = new CellTable<FoodTruckData>(50);
+	private ScrollPanel scrollPanel = new ScrollPanel(truckCellTable);
+	private ScrollPanel favScrollPanel = new ScrollPanel();
+
+	private CellList<String> truckCellList = 
+			new CellList<String>(new TextCell());
 	private Label lastUpdatedLabel = new Label();
 	private LoginInfo loginInfo = null;
 	private VerticalPanel loginPanel = new VerticalPanel();
@@ -78,17 +91,14 @@ public class FoodOnWheels implements EntryPoint {
 	private Label loginLabel = new Label(
 			"Sign in to customize your favourite food vendors list!");
 
-	private final FoodTruckServiceAsync foodTruckService = GWT
-			.create(FoodTruckService.class);
-	
+	/**
+	 * Remote remote service proxy to talk to the server-side FoodTruckService
+	 */
+	private final FoodTruckServiceAsync 
+	foodTruckService = GWT.create(FoodTruckService.class);
+
 	//Needed to create FoodCartMap object
 	FoodCartMap cartMap = new FoodCartMap(true);
-
-	/**
-	 * The list of data to display.
-	 */
-	private static final List<String> DAYS = Arrays.asList("Sunday\r\nagain",
-			"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
 
 	/**
 	 * This is the entry point method.
@@ -99,50 +109,135 @@ public class FoodOnWheels implements EntryPoint {
 		LoginServiceAsync loginService = GWT.create(LoginService.class);
 		loginService.login(GWT.getHostPageBaseURL(),
 				new AsyncCallback<LoginInfo>() {
-					public void onFailure(Throwable error) {
-					}
+			public void onFailure(Throwable error) {
+			}
 
-					public void onSuccess(LoginInfo result) {
-						loginInfo = result;
-						if (loginInfo.isLoggedIn()) {
-							loadFoodTruckDataList();
-						} else {
-							loadLogin();
-						}
-					}
-				});
+			public void onSuccess(LoginInfo result) {
+				loginInfo = result;
+				if (loginInfo.isLoggedIn()) {
+					loadFoodTruckDataList();
+				} else {
+					loadLogin();
+				}
+			}
+		});
 
+		//Needed to load map
 
+		Maps.loadMapsApi("AIzaSyCRBwxpSxylsp96KCX96xRHUQxrY6e653I", "2", false, new Runnable() {
+			public void run() {
+				cartMap.buildUi();
 
-		truckCellList
-				.setKeyboardSelectionPolicy(KeyboardSelectionPolicy.ENABLED);
+			}});
+
+//		setUpTruckCellTable();
+
+		truckCellList.setKeyboardSelectionPolicy(KeyboardSelectionPolicy.ENABLED);
 		// Add a selection model to handle user selection.
 		final SingleSelectionModel<String> selectionModel = new SingleSelectionModel<String>();
 		truckCellList.setSelectionModel(selectionModel);
 
-		selectionModel
-		.addSelectionChangeHandler(new SelectionChangeEvent.Handler() {
-			public void onSelectionChange
-			(SelectionChangeEvent event) {
-				String selected = selectionModel.getSelectedObject();
-				if (selected != null) {
-					Window.alert("You selected: " + selected);
-				}
-			}
-		});
-		
-		//Needed to load map
-		
-		Maps.loadMapsApi("AIzaSyCRBwxpSxylsp96KCX96xRHUQxrY6e653I", "2", false, new Runnable() {
-		public void run() {
-			cartMap.buildUi();
-
-}});
-		
-		
+		selectionModel.addSelectionChangeHandler(
+				new SelectionChangeEvent.Handler() {
+					public void onSelectionChange
+					(SelectionChangeEvent event) {
+						String selected = selectionModel.getSelectedObject();
+						if (selected != null) {
+							Window.alert("You selected: " + selected);
+						}
+					}
+				});
 	}
 
+	private void setUpTruckCellTable() {
+		// TODO Auto-generated method stub
+		truckCellTable.setKeyboardSelectionPolicy(KeyboardSelectionPolicy.ENABLED);
+		truckDataProvider.addDataDisplay(truckCellTable);
 
+		// Do not refresh the headers and footers every time the data is updated.
+//		truckCellTable.setAutoHeaderRefreshDisabled(true);
+//		truckCellTable.setAutoFooterRefreshDisabled(true);
+
+		// Attach a column sort handler to the ListDataProvider to sort the list.
+		ListHandler<FoodTruckData> 
+		sortHandler = new ListHandler<FoodTruckData>(truckDataProvider.getList());
+		truckCellTable.addColumnSortHandler(sortHandler);
+
+		// Add a text column to show the name.
+		TextColumn<FoodTruckData> 
+		nameColumn = new TextColumn<FoodTruckData>() {
+
+			@Override
+			public String getValue(FoodTruckData object) {
+				// TODO Auto-generated method stub
+				return object.getName();
+			}
+
+		};
+		nameColumn.setSortable(true);
+		
+
+		sortHandler.setComparator(nameColumn, 
+				new Comparator<FoodTruckData>(){
+
+			@Override
+			public int compare(FoodTruckData o1,
+					FoodTruckData o2) {
+				// TODO Auto-generated method stub
+				return o1.getName().compareTo(o2.getName());
+			}
+		});
+		truckCellTable.addColumn(nameColumn, "Food Truck Name");
+
+		TextColumn<FoodTruckData> 
+		userCountColumn = new TextColumn<FoodTruckData>() {
+
+			@Override
+			public String getValue(FoodTruckData object) {
+				// TODO Auto-generated method stub
+				return object.getCount();
+			}
+
+		};
+		userCountColumn.setSortable(true);
+		
+
+		sortHandler.setComparator(userCountColumn, 
+				new Comparator<FoodTruckData>(){
+
+			@Override
+			public int compare(FoodTruckData o1,
+					FoodTruckData o2) {
+				// TODO Auto-generated method stub
+				int diff = Integer.parseInt(o1.getCount()) 
+						- Integer.parseInt(o2.getCount());
+				return Integer.signum(diff);
+			}
+
+		});
+		truckCellTable.addColumn(userCountColumn,"Checkin Count");
+
+		// Add a selection model to handle user selection.
+		final SingleSelectionModel<FoodTruckData> 
+		selectionModel = new SingleSelectionModel<FoodTruckData>();
+
+		truckCellTable.setSelectionModel(selectionModel);
+
+		selectionModel.addSelectionChangeHandler(
+				new SelectionChangeEvent.Handler() {
+
+					@Override
+					public void onSelectionChange(SelectionChangeEvent event) {
+						// TODO Auto-generated method stub
+						FoodTruckData 
+						selected = selectionModel.getSelectedObject();
+
+						if (selected != null) {
+							Window.alert("You selected: " + selected);
+						}
+					}
+				});
+	}
 
 	private void loadLogin() {
 		// Assemble login panel.
@@ -151,90 +246,88 @@ public class FoodOnWheels implements EntryPoint {
 		loginPanel.add(signInLink);
 		RootPanel.get("foodTruckList").add(loginPanel);
 	}
-	
+
 	private void loadFoodTruckData() {
-		foodTruckService
-		.getFoodTruckDataList(new AsyncCallback<List<FoodTruckData>>() {
+		foodTruckService.getFoodTruckDataList(
+				new AsyncCallback<List<FoodTruckData>>() {
 
-			@Override
-			public void onFailure(Throwable caught) {
-				// TODO Auto-generated method stub
-				truckCellList.setRowCount(DAYS.size(), true);
-				// Push the data into the widget.
-				truckCellList.setRowData(0, DAYS);
+					@Override
+					public void onFailure(Throwable caught) {
+						Window.alert("Failed to get truck data from server.");
+					}
 
-			}
+					@Override
+					public void onSuccess(List<FoodTruckData> result) {
+						//TODO
+						List<String> values = new ArrayList<String>();
+						for (int k = 0; k < result.size(); k++)
+							values.add(result.get(k).toString());
+						truckCellList.setRowCount(values.size(), true);
+						truckCellList.setRowData(0, values);
 
-			@Override
-			public void onSuccess(List<FoodTruckData> result) {
-				// TODO Auto-generated method stub
-				List<String> values = new ArrayList<String>();
-				for (int k = 0; k < result.size(); k++)
-					values.add(result.get(k).toString());
-				truckCellList.setRowCount(values.size(), true);
-				truckCellList.setRowData(0, values);
-			}
-		});
+						truckDataProvider.setList(result);
+						setUpTruckCellTable();
+//						truckCellTable.setRowCount(values.size(), true);
+//						truckCellTable.setRowData(0, result);
+					}
+				});
 	}
-	
+
 	private void loadFoodTruckDataList() {
 		// Set up sign out hyperlink.
 		signOutLink.setHref(loginInfo.getLogoutUrl());
-				
+
 		loadFoodTruckData();
-		
+
 		// Push the data into the widget.
 		truckListPanel.add(fetchTruckListButton);
-		truckListPanel.add(truckCellList);
+		
+		scrollPanel.setSize("500px", "400px");
+//		truckListPanel.add(scrollPanel);
+		tabLayout.add(scrollPanel, "[All Food Trucks]");
+		tabLayout.add(favScrollPanel, "[Favourites]");
+		tabLayout.setAnimationDuration(1000);
+		tabLayout.getElement().getStyle().setMargin(10, Unit.PX);
+		tabLayout.setSize("500px", "400px");
+		truckListPanel.add(tabLayout);
+//		DecoratorPanel decoratorPanel = new DecoratorPanel();
+//		decoratorPanel.add(scrollPanel);
+//		truckListPanel.add(decoratorPanel);
+		//		truckListPanel.add(truckCellList);
 		truckListPanel.add(lastUpdatedLabel);
 		truckListPanel.add(signOutLink);
-		
+
 		// Add it to the root panel.
 		RootPanel.get("foodTruckList").add(truckListPanel);
 
 		// Listen for mouse events on the fetch YELP data button.
-		fetchTruckListButton.addClickHandler(new ClickHandler() {
-			public void onClick(ClickEvent event) {
-				fetchDataFromProvider();
-			};
-		});
+		fetchTruckListButton.addClickHandler(
+				new ClickHandler() {
+					public void onClick(ClickEvent event) {
+						fetchDataFromProvider();
+					};
+				});
 	}
-	
+
 	/**
-	 * Fetch food truck data from YELP. Executed when the user clicks
-	 * fetchTruckListButton.
+	 * Fetch food truck data from provider. Executed when the user clicks
+	 * fetchTruckListButton. This should be only accessible to the admin.
 	 */
 	private void fetchDataFromProvider() {
-//		foodTruckService
-//		.getFoodTruckFilterName("Japadog", 
-//				new AsyncCallback<List<FoodTruckData>>(){
-//
-//			@Override
-//			public void onFailure(Throwable caught) {
-//				// TODO Auto-generated method stub
-//
-//			}
-//
-//			@Override
-//			public void onSuccess(List<FoodTruckData> result) {
-//				// TODO Auto-generated method stub
-//				Window.alert("Results from FourSquare stored into server.");
-//			}
-//		});
-		foodTruckService
-		.fetchFoodTruckDataFromFourSquare(new AsyncCallback<Void>() {
+		foodTruckService.fetchFoodTruckDataFromFourSquare(
+				new AsyncCallback<Void>() {
 
-			@Override
-			public void onFailure(Throwable caught) {
-				Window.alert(caught.getMessage());
-			}
+					@Override
+					public void onFailure(Throwable caught) {
+						Window.alert(caught.getMessage());
+					}
 
-			@Override
-			public void onSuccess(Void result) {
-				Window.alert("Results from FourSquare stored into server.");
-				loadFoodTruckDataList();
-			}
-
-		});
+					@Override
+					public void onSuccess(Void result) {
+						Window.alert("Food truck data from provider stored " 
+								+ "into app engine datastore.");
+						loadFoodTruckDataList();
+					}
+				});
 	}
 }
